9c97e13effa04d695ec24a8668fc395cbd2dc4de
poleomanta <poleomenta@tuta.com>
2025-11-22 19:07:17 +0100

    add amenities test to api

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,13 +1,18 @@
 from destinations import getDestinations
+from amenities import getAmenityDataByLocation
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/api/destinations', methods=['GET'])
-def test():
+def test_destinations():
   sights = getDestinations()
-
   return jsonify([sight.toJSON().json for sight in sights])
+
+@app.route('/api/amenities', methods=['GET'])
+def test_amenities():
+  data = getAmenityDataByLocation(48.134672, 11.568834)
+  return jsonify([group.toJSON().json for group in data])
 
 if __name__ == '__main__':
   app.run(debug=True, port=8080)
