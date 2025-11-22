d17ee2919cd0e4f0e1460d46688a69c8c818f808
poleomanta <poleomenta@tuta.com>
2025-11-22 15:32:36 +0100

    Amenity search a bit more structured

--- src/backend/amenities.py
+++ src/backend/amenities.py
@@ -4,27 +4,21 @@ from enum import Enum
 from typing import Final
 from pathlib import Path
 import os
-
+from destinations import Destination
 import osmnx as ox
+from geopandas import GeoDataFrame
 
 
-# WC, trinkwasser, spielplatz, benches and parks
+@dataclass
+class Location:
+    lat: float
+    lon: float
 
-# Separate querying from finding logic
-# Amenity types
 class AmenityType(str, Enum):
     WC = "WC"
     FOUNTAIN = "FOUNTAIN"
     SPIELPLATZ = "SPIELPLATZ"
     PARK = "PARK"
-
-# TODO consider if class with more info location specific
-@dataclass
-class Location:
-    # By default the datasets use
-    # https://en.wikipedia.org/wiki/European_Terrestrial_Reference_System_1989
-    lat: float
-    lon: float
 
 """ Goal is to convey how good is the amenity status of a given type"""
 class AmenityStatus(str, Enum):
@@ -41,40 +35,21 @@ class AmenityGroup:
     status: AmenityStatus
     locations: list[Location] = field(default_factory=list)
 
-def point_to_location(entry: str) -> Location:
-    # POINT (692441.0257008562 5331488.180897921)
-    print(entry)
-    print(entry[6:-1].split(' '))
-    (lat, lon) = entry[6:-1].split(' ')
-    return Location(lat=lat, lon=lon)
-
-def getFountainLocations() -> AmenityGroup:
-    """Gets all fountains from file"""
-    # Build query for desired objects within the area
-    query = overpassQueryBuilder(
-        elementType='node',
-        area=areaId,
-        selector='"amenity"',
-        out='body'
-    )
-
-    result = overpass.query(query)
-    wcs = result.elements()
-
-    for wc in wcs:
-        tags = wc.tags()
-        print('---')
-        print(tags.get('name'))
-        print(tags.get('amenity'))
-        print(wc.lat())
-        print(wc.lon())
+def getAllAmenityData(address: str, radius: int = 300):
+    """ TODO
+    - parameters?
+     """
+    try:
+        return ox.features.features_from_address(
+            address, {'amenity': True}, dist=radius)
+    except InsufficientResponseError:
+        return []
 
 
-# get building details from address within 1000 m
+def getWCData(df: GeoDataFrame) -> AmenityGroup:
+    # TODO exception
+    df2 = df[df['amenity'].isin(['toilets'])]
+    
 place = "Viktualienmarkt, Munich, Germany"
-gdf = ox.features.features_from_address(
-    place, {'amenity': 'bench'}, dist=300)
-
-# print first 5 building details
-print(gdf.head(5))
-
+df = getAllAmenityData(place)
+getWCData(df)
